0e3c2f9e290ee50e18ac722f8ee6c799d2c3331c
mr678 <matthias.reining@outlook.com>
2013-08-13 20:51:12 +0200

    vacation add weekend information

--- src/main/scripts/individual-roles.sql
+++ src/main/scripts/individual-roles.sql
@@ -1,3 +1,3 @@
-DELETE from app.INDIVIDUALROLE;
-INSERT INTO app.INDIVIDUALROLE(ID, rolename, description) VALUES (1, 'VACATION_MANAGER', 'Users with this role can approve or reject a request for time off');
+DELETE from INDIVIDUALROLE;
+INSERT INTO INDIVIDUALROLE(ID, rolename, description) VALUES (1, 'VACATION_MANAGER', 'Users with this role can approve or reject a request for time off');
 
