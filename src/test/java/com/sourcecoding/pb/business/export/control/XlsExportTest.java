f3a88f69a518def95c193fd8ef13e34b145a34f7
mr678 <matthias.reining@outlook.com>
2013-09-24 11:29:20 +0200

    bugfix linux

--- src/test/java/com/sourcecoding/pb/business/export/control/XlsExportTest.java
+++ src/test/java/com/sourcecoding/pb/business/export/control/XlsExportTest.java
@@ -20,17 +20,6 @@ import org.junit.Ignore;
  *
  * @author Matthias
  */
-@Ignore
-/**
- * FIXME: wirft bei Cloudbees folgende Fehlermeldung
- * java.lang.ArrayIndexOutOfBoundsException: null
- * [INFO] 	at java.lang.System.arraycopy(Native Method)
- * [INFO] 	at jxl.biff.StringHelper.getBytes(StringHelper.java:127)
- * [INFO] 	at jxl.write.biff.WriteAccessRecord.<init>(WriteAccessRecord.java:59)
- * [INFO] 	at jxl.write.biff.WritableWorkbookImpl.write(WritableWorkbookImpl.java:726)
- * [INFO] 	at com.sourcecoding.pb.business.export.control.XlsExport.run(XlsExport.java:40)
- * [INFO] 	at com.sourcecoding.pb.business.export.control.XlsExportTest.run(XlsExportTest.java:34)
- */
 public class XlsExportTest {
 
     @Test 
@@ -41,6 +30,7 @@ public class XlsExportTest {
         //File f = new File("d:\\out.xls");
         OutputStream out = new ByteArrayOutputStream();
         Workbook template = Workbook.getWorkbook(this.getClass().getResourceAsStream("/vacation-template-test.xls"));
+        
 
         XlsExport xlsExport = new XlsExport();
         xlsExport.run(json, template, out);
