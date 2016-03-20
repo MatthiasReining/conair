e737a0249c8deadffc841ddf950ae02cf9f80b0c
Matthias Reining <matthias.reining@outlook.com>
2016-03-20 17:51:27 +0100

    change ports

--- src/main/java/com/sourcecoding/pb/business/mail/boundary/MailService.java
+++ src/main/java/com/sourcecoding/pb/business/mail/boundary/MailService.java
@@ -39,19 +39,24 @@ public class MailService {
 
         Properties props = new Properties();
 
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        //props.put("mail.smtp.host", "smtp.gmail.com");
+        //props.put("mail.smtp.socketFactory.port", "465");
+        //props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        //props.put("mail.smtp.auth", "true");
+        //props.put("mail.smtp.port", "465");
 
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+     
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
-                    @Override
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
 
         try {
 
---
+++ src/test/java/com/sourcecoding/pb/business/mail/boundary/MailServiceTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sourcecoding.pb.business.mail.boundary;
+
+import java.io.IOException;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ *
+ * @author Matthias Reining
+ */
+@Ignore
+public class MailServiceTest {
+
+    @Test
+    public void shouldSendMeAnEMail() throws IOException {
+        System.out.println("blub");
+        //GmailQuickStart.authorize();
+        MailService ms = new MailService();
+        ms.send("matthias.reining@outlook.com", "Test-Mail von ConAIR", "blub blu");
+
+    }
+
+}
